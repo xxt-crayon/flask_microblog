6ab020ebc1ac6401e2d8694313bb8f042176b5d2
xxt-crayon <1731378786@qq.com>
2022-09-18 23:02:58 +0800

    chapter2 template finished

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,5 @@
+from flask import render_template
+
 # 从app包中导入 app这个实例
 from app import app
 
@@ -7,4 +9,15 @@ from app import app
 @app.route('/index')
 # 1个视图函数
 def index():
-    return "Hello,World!"  # 返回一个字符串
+    user = {'username': 'xxt'}
+    posts = [  # 创建一个列表：帖子。里面元素是两个字典，每个字典里元素还是字典，分别作者、帖子内容。
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', user=user, posts=posts)
