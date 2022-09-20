fcee044d970cc2b70768cb041b93f39e01d7e731
xxt-crayon <1731378786@qq.com>
2022-09-20 16:06:40 +0800

    chapter04 databases finished

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,17 @@
 from flask import Flask
 from config import Config
 
+from flask_sqlalchemy import SQLAlchemy#从包中导入类
+from flask_migrate import Migrate
+
 # 将Flask类的实例赋值名为 app 的变量。这个实例成为app包的成员
 app = Flask(__name__)
 app.config.from_object(Config)
 
 # print('等会谁（哪个包或模块）在使用我：', __name__)
 
+db = SQLAlchemy(app)#数据库对象
+migrate = Migrate(app, db)#迁移引擎对象
 
 # 从app包中导入模块routes
-from app import routes
+from app import routes, models
